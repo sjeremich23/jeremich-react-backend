d7cb78b54d8b625b5c35fbb2e1b48b8462847970
Shane Jeremich <shane.jeremich@gmail.com>
2020-01-28 22:10:56 -0700

    deleted api folders

--- template/api/middleware/logger.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res, next) => {
-  const { method, url } = req;
-
-  console.log(
-    `[${new Date().toLocaleTimeString()}] ${method} request to URL: ${url}`
-  );
-  next();
-};
--- template/index.js
+++
@@ -1,7 +0,0 @@
-const server = require("./api/server");
-
-const PORT = process.env.PORT || 4000;
-
-server.listen(PORT, () => {
-  console.log(`\n*** Server is running on: http://localhost:${PORT} ***`);
-});
